a4971c3bf7e974a0dff3a198d1e822f88cbbe3b1
Scott Colby <scolby33@gmail.com>
2017-01-05 19:28:01 -0800

    A very simple PyBEL extension

---
+++ src/pybel_test_extension/__init__.py
@@ -0,0 +1,14 @@
+"""An example of how to implement a PyBEL extension."""
+
+__version__ = '0.1.0'
+
+__title__ = 'PyBEL Test Extension'
+__description__ = 'An exmaple of how to implement a PyBEL extension.'
+__url__ = 'https://github.com/pybel/pybel-test-extension'
+
+__author__ = 'Scott Colby'
+__email__ = 'scolby33@gmail.com'
+
+__license__ = 'Apache 2.0 License'
+__copyright__ = 'Copyright (c) 2016 Scott Colby'
+
---
+++ src/pybel_test_extension/extension.py
@@ -0,0 +1,2 @@
+def an_extension_function():
+    return 42  # a fair dice roll?
