34870b9de1dd8c086f6635baba4f25ec107a1154
Scott Colby <scolby33@gmail.com>
2017-01-05 19:50:11 -0800

    Fix some spelling errors--sorry, Charlie!

--- src/pybel_test_extension/__init__.py
+++ src/pybel_test_extension/__init__.py
@@ -3,11 +3,11 @@
 __version__ = '0.1.0'
 
 __title__ = 'PyBEL Test Extension'
-__description__ = 'An exmaple of how to implement a PyBEL extension.'
+__description__ = 'An example of how to implement a PyBEL extension.'
 __url__ = 'https://github.com/pybel/pybel-test-extension'
 
 __author__ = 'Scott Colby'
 __email__ = 'scolby33@gmail.com'
 
 __license__ = 'Apache 2.0 License'
-__copyright__ = 'Copyright (c) 2017 Scott Colby, Charles Tapely Hoyt'
+__copyright__ = 'Copyright (c) 2017 Scott Colby, Charles Tapley Hoyt'
